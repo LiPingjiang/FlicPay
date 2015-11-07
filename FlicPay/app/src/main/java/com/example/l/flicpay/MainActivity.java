1dfc22d578a71ef719dc171d7ad60630059481e3
Juan Camilo García <juancamilo87@outlook.com>
2015-11-07 15:27:20 +0200

    Password recording

--- FlicPay/app/src/main/java/com/example/l/flicpay/MainActivity.java
+++ FlicPay/app/src/main/java/com/example/l/flicpay/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.l.flicpay;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Message;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.Transformation;
 import android.widget.Button;
 
 import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextSwitcher;
 
-import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
 
 import io.flic.lib.FlicButton;
 import io.flic.lib.FlicButtonCallback;
@@ -21,19 +26,28 @@ import io.flic.lib.FlicManager;
 import io.flic.lib.FlicManagerInitializedCallback;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private int area;
-    private int[] passward= new int[10];
-    private Button Button_Input;
+    private int[] password = new int[10];
     private FlicManager manager;
+    private ImageView background;
+    private View pass_nums;
+    private Button btn_start;
+    private int counter;
+    private TextSwitcher txt_counter;
+    private List<FlicButton> buttons;
+    private HashMap<String,FlicButton> buttons_map;
+
+    private boolean recording;
     String TAG ="flicpay";
     private FlicButtonCallback buttonCallback = new FlicButtonCallback() {
         @Override
         public void onButtonUpOrDown(FlicButton button, boolean wasQueued, int timeDiff, boolean isUp, boolean isDown) {
             final String text = button + " was " + (isDown ? "pressed" : "released");
             Log.d(TAG, text);
-            passward[area]++;
+            if(recording)
+                password[area]++;
         }
     };
     private void stuffToBeExecuted(){
@@ -44,45 +58,15 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        recording = false;
         setContentView(R.layout.activity_main);
 
-        Button_Input = (Button)findViewById(R.id.button);
-        Button_Input.setOnClickListener(new View.OnClickListener() {
+        btn_start = (Button)findViewById(R.id.button);
+        btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Click input pin button, start handler");
-
-                area = -1;
-                for( int i:passward){
-                    i=0;
-                }
-
-                final Handler handler = new Handler();
-
-                final Runnable r = new Runnable() {
-                    public void run() {
-                        area++;
-                        if(area<10){
-                            Log.d(TAG, "second:"+ area);
-                            handler.postDelayed(this, 1000);}
-                        else{
-                            Log.d(TAG,"password:"
-                                    +"[0]"+passward[0]+","
-                                    +"[1]"+passward[1]+","
-                                    +"[2]"+passward[2]+","
-                                    +"[3]"+passward[3]+","
-                                    +"[4]"+passward[4]+","
-                                    +"[5]"+passward[5]+","
-                                    +"[6]"+passward[6]+","
-                                    +"[7]"+passward[7]+","
-                                    +"[8]"+passward[8]+","
-                                    +"[9]"+passward[9]+".");
-                        }
-
-                    }
-                };
-
-                handler.postDelayed(r, 0);
+                btn_start.setEnabled(false);
+                startAnimation();
             }
         });
 
@@ -92,9 +76,23 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onInitialized(FlicManager manager) {
                 MainActivity.this.manager = manager;
-                manager.initiateGrabButton(MainActivity.this);// this refers to the current Activity.
+                buttons = manager.getKnownButtons();
+                for (FlicButton button : buttons) {
+                    buttons_map.put(button.getButtonId(), button);
+                    setButtonCallback(button);
+                }
+//                manager.initiateGrabButton(MainActivity.this);// this refers to the current Activity.
             }
         });
+
+
+
+        counter = 3;
+        background = (ImageView) findViewById(R.id.img_background);
+        pass_nums = findViewById(R.id.pass_nums);
+        txt_counter = (TextSwitcher) findViewById(R.id.counter);
+        txt_counter.setInAnimation(this, R.anim.fade_in);
+        txt_counter.setOutAnimation(this, R.anim.fade_out);
 
 
     }
@@ -143,5 +141,128 @@ public class MainActivity extends ActionBarActivity {
     protected void onDestroy() {
         FlicManager.destroyInstance();
         super.onDestroy();
+    }
+
+    private void startAnimation()
+    {
+
+        final ResizeAnimation resizeAnimation = new ResizeAnimation(background, pass_nums.getWidth());
+        resizeAnimation.setDuration(11000);
+        resizeAnimation.setInterpolator(new LinearInterpolator());
+        resizeAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                btn_start.setEnabled(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                txt_counter.setOutAnimation(getApplicationContext(), R.anim.fade_out);
+                if(counter>0)
+                {
+
+                    txt_counter.setText(counter + "");
+                    counter--;
+                    handler.postDelayed(this,1000);
+                }
+                else
+                {
+                    txt_counter.setText("Start");
+                    background.startAnimation(resizeAnimation);
+
+                    //Record password
+                    Log.d(TAG, "Click input pin button, start handler");
+
+                    area = -1;
+                    for(int i = 0; i<password.length; i++){
+                        password[i]=0;
+                    }
+
+                    final Handler handler_pass = new Handler();
+
+                    final Runnable r = new Runnable() {
+                        public void run() {
+                            area++;
+                            if(area<10){
+                                recording = true;
+                                Log.d(TAG, "second:"+ area);
+                                handler_pass.postDelayed(this, 1000);}
+                            else{
+                                recording = false;
+                                Log.d(TAG,"password:"
+                                        +"[0]"+ password[0]+","
+                                        +"[1]"+ password[1]+","
+                                        +"[2]"+ password[2]+","
+                                        +"[3]"+ password[3]+","
+                                        +"[4]"+ password[4]+","
+                                        +"[5]"+ password[5]+","
+                                        +"[6]"+ password[6]+","
+                                        +"[7]"+ password[7]+","
+                                        +"[8]"+ password[8]+","
+                                        +"[9]"+ password[9]+".");
+                            }
+
+                        }
+                    };
+
+                    handler_pass.postDelayed(r, 1000);
+
+                    counter--;
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            txt_counter.setOutAnimation(getApplicationContext(), R.anim.long_fade_out);
+                            txt_counter.setText("");
+                            counter = 3;
+                        }
+                    },800);
+                }
+            }
+        };
+
+        handler.post(runnable);
+
+    }
+
+    public class ResizeAnimation extends Animation {
+        final int startWidth;
+        final int targetWidth;
+        View view;
+
+        public ResizeAnimation(View view, int targetWidth) {
+            this.view = view;
+            this.targetWidth = targetWidth;
+            startWidth = view.getWidth();
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            int newWidth = (int) (targetWidth * interpolatedTime);
+            view.getLayoutParams().width = newWidth;
+            view.requestLayout();
+        }
+
+        @Override
+        public void initialize(int width, int height, int parentWidth, int parentHeight) {
+            super.initialize(width, height, parentWidth, parentHeight);
+        }
+
+        @Override
+        public boolean willChangeBounds() {
+            return true;
+        }
     }
 }
